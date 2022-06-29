a6a46540f2a55aa5f4a72cec50cbe901395f11cd
champ09D <darshanmhatre709@gmail.com>
2022-06-29 15:33:11 +0530

    Gambling Problem UC1 Uploaded Successfully

---
+++ gambling_problem_UC1.java
@@ -0,0 +1,11 @@
+public class gambling_problem_UC1 {
+    public static void main(String[] darsh) {
+
+        int stake_Amount = 100;
+        int bet = 1;
+        System.out.println("-------Welcome to the Gambling Problem------");
+        System.out.println("Amount of stake to start game  of every day is " + stake_Amount);
+        System.out.println("Amount to bet after every game " + bet);
+
+    }
+}
